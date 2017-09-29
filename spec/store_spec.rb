64b76cb9f97253b9948ce4d65badf6d4ee9d1fcb
Dan Kiss <flowfast47@gmail.com>
2017-09-29 15:53:27 -0700

    add passing spec for active record relation

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,8 +1,21 @@
 require('spec_helper')
 
-describe(Shoe) do
-  it { should have_many(:stores, through )}
+describe (Store) do
+  describe('#store') do
+    it('adds a store to a shoe') do
+      store = Store.create(:name => 'downtown_store')
+      shoe1 = Shoe.create(:brand => 'nike', :price => '$50.00')
+      shoe2 = Shoe.create(:brand => 'adidas', :price => '$40.00')
+      store.shoes.push(shoe1)
+      store.shoes.push(shoe2)
+      expect(store.shoes).to eq([shoe1, shoe2])
+    end
+
+  end
 
 end
 
-Dir.pwd()
+
+# describe(Shoe) do
+#   it { should have_many(:stores) }
+# end
