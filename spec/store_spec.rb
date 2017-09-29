d17cfc351eecc09dfea5ae9ddfd90adeb2c6bda1
Dan Kiss <flowfast47@gmail.com>
2017-09-29 16:00:42 -0700

    add passing specs that shows active record relations between tables are working

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -4,10 +4,11 @@ describe (Store) do
   describe('#store') do
     it('adds a store to a shoe') do
       store = Store.create(:name => 'downtown_store')
-      shoe1 = Shoe.create(:brand => 'nike', :price => '$50.00')
-      shoe2 = Shoe.create(:brand => 'adidas', :price => '$40.00')
+      shoe1 = Shoe.create(:brand => 'nike', :price => '50')
+      shoe2 = Shoe.create(:brand => 'adidas', :price => '40')
       store.shoes.push(shoe1)
       store.shoes.push(shoe2)
+      # binding.pry
       expect(store.shoes).to eq([shoe1, shoe2])
     end
 
@@ -16,6 +17,15 @@ describe (Store) do
 end
 
 
-# describe(Shoe) do
-#   it { should have_many(:stores) }
-# end
+describe(Shoe) do
+  it { should have_many(:stores) }
+end
+
+describe(Store) do
+  it { should have_many(:shoes)}
+end
+
+describe(Catalogue) do
+  it { should belong_to(:shoe)}
+  it { should belong_to(:store)}
+end
