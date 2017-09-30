8781a1e23e5c6edee028bba5529453a9c43fa017
Dan Kiss <flowfast47@gmail.com>
2017-09-29 17:14:59 -0700

    add new route, not finished

--- app.rb
+++ app.rb
@@ -10,8 +10,14 @@ get('/') do
 end
 
 post('/') do
+  @stores = Store.all()
   store_name = params.fetch("store_name")
   Store.create({:name => store_name})
   # @stores = Store.all()
   redirect '/'
 end
+
+# get('/store/:id') do
+#   @store = Store.find(params[:id])
+#
+# end
